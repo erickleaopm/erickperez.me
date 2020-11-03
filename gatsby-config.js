96a6fa75f206d1fd69aa0530c87c073bbd6265fa
Erick Pérez <erick.leao.perez@gmail.com>
2020-11-03 00:55:42 -0700

    refactor: change configuration for gatsby to consume strapi api and update dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,10 +6,10 @@
 
 module.exports = {
   siteMetadata: {
-    title: "WebDev Portfolio",
-    description: "This is WebDev Portfolio Site",
-    author: "@webdev",
-    twitterUsername: "@john_smilga",
+    title: "Erick Perez Portfolio",
+    description: "This is Erick Perez Portfolio Site",
+    author: "Erick Pérez",
+    twitterUsername: "@erickleaopm",
     image: "/twitter-img.png",
     siteUrl: "https://testing-strapi-gatsby-build.netlify.app",
   },
@@ -33,7 +33,7 @@ module.exports = {
         //   contentTypes : `jobs`, `projects`, `blogs`,
         //   singleType : `about`
         //  ONLY ADD TO ARRAY IF YOU HAVE DATA IN STRAPI !!!!
-        contentTypes: [],
+        contentTypes: [`jobs`],
         singleTypes: [],
       },
     },
