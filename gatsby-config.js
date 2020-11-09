050cc15e426a0daf2f948b7d6a2d8c54c2e851e0
Erick Pérez <erick@tvpacifico.mx>
2020-11-09 15:50:20 -0700

    feat: added about page content

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,10 +31,10 @@ module.exports = {
         apiURL: `http://localhost:1337`,
         queryLimit: 1000, // Default to 100
         //   contentTypes : `jobs`, `projects`, `blogs`,
-        //   singleType : `about`
+        //   singleTypes : `about`
         //  ONLY ADD TO ARRAY IF YOU HAVE DATA IN STRAPI !!!!
         contentTypes: [`jobs`, `projects`],
-        singleTypes: [],
+        singleTypes: [`about`],
       },
     },
     // {
